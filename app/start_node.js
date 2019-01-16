280c7bc1f8c165233904aeaa2b5db4b28fe2da62
ajgosling <ajgosling2@gmail.com>
2019-01-16 15:07:34 -0800

    working on wikipedia

--- app/start_node.js
+++ app/start_node.js
@@ -1,12 +1,12 @@
 wtf.fetch(['Royal Cinema', 'Aldous Huxley'], 'en', {
   'Api-User-Agent': 'spencermountain@gmail.com'
 }).then((docList) => {
-  let infoboxes = docList.map(doc => {
-    return {
-      title: doc.title(),
-      infobox: doc.infoboxes(0)
-    }
-  });
-  Document.getElementById(
-    
+  // let infoboxes = docList.map(doc => {
+  //   return {
+  //     title: doc.title(),
+  //     infobox: doc.infoboxes(0)
+  //   }
+  // });
+  console.log("i'm here")
+  document.getElementById("mytext").innerHTML = "it worked!"
 });
