fd5aa0c1a3b34a8f8485ffded2f711fea310e674
ajgosling <ajgosling2@gmail.com>
2019-02-11 19:08:18 -0800

    change start_node

--- app/start_node.js
+++ app/start_node.js
@@ -1,12 +1,13 @@
-wtf.fetch(['Royal Cinema', 'Aldous Huxley'], 'en', {
-  'Api-User-Agent': 'spencermountain@gmail.com'
-}).then((docList) => {
-  // let infoboxes = docList.map(doc => {
-  //   return {
-  //     title: doc.title(),
-  //     infobox: doc.infoboxes(0)
-  //   }
-  // });
-  console.log("i'm here")
-  document.getElementById("mytext").innerHTML = "it worked!"
-});
+// wtf.fetch(['Royal Cinema', 'Aldous Huxley'], 'en', {
+//   'Api-User-Agent': 'spencermountain@gmail.com'
+// }).then((docList) => {
+//   // let infoboxes = docList.map(doc => {
+//   //   return {
+//   //     title: doc.title(),
+//   //     infobox: doc.infoboxes(0)
+//   //   }
+//   // });
+//   console.log("i'm here")
+//   document.getElementById("mytext").innerHTML = "it worked!"
+// });
+console.log(wtf.fetch('2018–19_Toronto_Raptors_season').then(doc=> doc.section('Roster').json()))
