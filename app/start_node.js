39ca6fb2f12ae2e9d1793f3f48f720179769eab7
ajgosling <ajgosling2@gmail.com>
2019-02-12 17:53:15 -0800

    working button

--- app/start_node.js
+++ app/start_node.js
@@ -1,34 +1,17 @@
-// wtf.fetch(['Royal Cinema', 'Aldous Huxley'], 'en', {
-//   'Api-User-Agent': 'spencermountain@gmail.com'
-// }).then((docList) => {
-//   // let infoboxes = docList.map(doc => {
-//   //   return {
-//   //     title: doc.title(),
-//   //     infobox: doc.infoboxes(0)
-//   //   }
-//   // });
-//   console.log("i'm here")
-//   document.getElementById("mytext").innerHTML = "it worked!"
-// });
 async function start() {
   let middleSection = document.getElementById("test")
-  var h = document.createElement("li");
-  let g = document.createTextNode("first node");
-  h.appendChild(g);
-  middleSection.appendChild(h);
-  // var node;
-  wtf.fetch('On a Friday', 'en', function(err, doc) {
-    var val = doc.infobox(0).get('current_members');
-    val.links().forEach(link => { 
-      var node = document.createElement("li")
-      let text = document.createTextNode(link.page);
-      node.appendChild(text);
-      middleSection.appendChild(node);
-      // middleSection.appendChild(document.createTextNode("hello"));
+
+  wtf.fetch('Abraham Lincoln', 'en', function(err, doc) {
+    doc.links().forEach(link => { 
+      if (link.page) {
+        var node = document.createElement("li")
+        let text = document.createTextNode(link.page);
+        node.appendChild(text);
+        middleSection.appendChild(node);
+      }
     });
-    //['Thom Yorke', 'Jonny Greenwood', 'Colin Greenwood'...]
-    console.log(val)
   });
 }
 
-start();
+let startButton = document.getElementById("start");
+startButton.onclick = start;
