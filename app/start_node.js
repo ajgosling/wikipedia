5b84897842b72ea8d2cd281a0a7f0e13b29d7a9f
ajgosling <ajgosling2@gmail.com>
2019-02-12 18:05:13 -0800

    working entry field

--- app/start_node.js
+++ app/start_node.js
@@ -1,7 +1,9 @@
 async function start() {
-  let middleSection = document.getElementById("test")
+  let middleSection = document.getElementById("test");
 
-  wtf.fetch('Abraham Lincoln', 'en', function(err, doc) {
+  let startInput = document.getElementById("start-input");
+
+  wtf.fetch(startInput.value, 'en', function(err, doc) {
     doc.links().forEach(link => { 
       if (link.page) {
         var node = document.createElement("li")
