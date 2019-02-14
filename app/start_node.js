54fa84dbb18684e4656df52d87a68a4e39179974
ajgosling <ajgosling2@gmail.com>
2019-02-13 21:17:59 -0800

    working error message

--- app/start_node.js
+++ app/start_node.js
@@ -4,8 +4,10 @@ async function start() {
   let startInput = document.getElementById("start-input");
 
   wtf.fetch(startInput.value, 'en', function(err, doc) {
-    if (err) {
-      document.getElementById("error-message").innerHTML("i'm an error");
+    if (!doc) {
+      console.log("inside");
+      let text = document.createTextNode("invalid search");
+      document.getElementById("error-message").appendChild(text);
     } else {
       doc.links().forEach(link => { 
         if (link.page) {
