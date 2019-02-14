8fd37d1b8928253a0aef1bf046d5958394561dfa
ajgosling <ajgosling2@gmail.com>
2019-02-13 21:14:06 -0800

    error messages

--- app/start_node.js
+++ app/start_node.js
@@ -4,14 +4,18 @@ async function start() {
   let startInput = document.getElementById("start-input");
 
   wtf.fetch(startInput.value, 'en', function(err, doc) {
-    doc.links().forEach(link => { 
-      if (link.page) {
-        var node = document.createElement("li")
-        let text = document.createTextNode(link.page);
-        node.appendChild(text);
-        middleSection.appendChild(node);
-      }
-    });
+    if (err) {
+      document.getElementById("error-message").innerHTML("i'm an error");
+    } else {
+      doc.links().forEach(link => { 
+        if (link.page) {
+          var node = document.createElement("li")
+          let text = document.createTextNode(link.page);
+          node.appendChild(text);
+          middleSection.appendChild(node);
+        }
+      });
+    }
   });
 }
 
