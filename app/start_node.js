f08df73596b0e1ee3eb6084f10490473e14bae18
AJ Gosling <ajgosling2@gmail.com>
2019-09-28 22:37:54 -0700

    getting going

--- app/start_node.js
+++ app/start_node.js
@@ -1,24 +1,26 @@
-function start() {
+async function start() {
   let middleSection = document.getElementById("test");
 
   let startInput = document.getElementById("start-input");
-
-  wtf.fetch(startInput.value, 'en', function(err, doc) {
-    if (!doc) {
-      console.log("inside");
-      let text = document.createTextNode("invalid search");
-      document.getElementById("error-message").appendChild(text);
-    } else {
-      doc.links().forEach(link => {
-        if (link.page) {
-          var node = document.createElement("li")
-          let text = document.createTextNode(link.page);
-          node.appendChild(text);
-          middleSection.appendChild(node);
-        }
-      });
-    }
-  });
+  console.log(startInput);
+  let doc = await wtf.fetch("Gold", "en");
+  console.log(doc);
+  // wtf.fetch(startInput.value, 'en', function(err, doc) {
+  //   if (!doc) {
+  //     console.log("inside");
+  //     let text = document.createTextNode("invalid search");
+  //     document.getElementById("error-message").appendChild(text);
+  //   } else {
+  //     doc.links().forEach(link => {
+  //       if (link.page) {
+  //         var node = document.createElement("li")
+  //         let text = document.createTextNode(link.page);
+  //         node.appendChild(text);
+  //         middleSection.appendChild(node);
+  //       }
+  //     });
+  //   }
+  // });
 
   let svg = d3
     .select("#wikiverse")
