76151ce78aa656ffbd0548ff715871be0668850a
AJ Gosling <ajgosling2@gmail.com>
2019-11-17 22:28:50 -0800

    miserables graph is on second svg

--- app/start_node.js
+++ app/start_node.js
@@ -1,17 +1,15 @@
 async function start() {
   let startName = document.querySelector('input').value;
   let doc = await wtf.fetch(startName, "en");
-  console.log(doc.links());
   var middleText = document.querySelector("#middle");
 
-  console.log(middleText);
 
-  var svg = d3.select("svg");
+  var svg = d3.select(".legit");
+  console.log(svg);
   var width = +svg.attr("width");
   var height = +svg.attr("height");
 
-  console.log(width);
-  console.log(height);
+  // color is a function that returns a hexadecimal color when invoked
   var color = d3.scaleOrdinal(d3.schemeCategory20);
 
   var simulation = d3.forceSimulation()
@@ -22,6 +20,9 @@ async function start() {
   d3.json("miserables.json", function (error, graph) {
       if (error) throw error;
 
+      var linksNames = doc.links();
+      console.log("wiki", linksNames);
+      console.log("graph", graph.nodes);
       var link = svg.append("g")
           .attr("class", "links")
           .selectAll("line")
@@ -93,11 +94,114 @@ async function start() {
 
 }
 
+async function startSecond() {
+
+  let startName = document.querySelector('input').value;
+  let doc = await wtf.fetch(startName, "en");
+  var middleText = document.querySelector("#middle");
+
+
+  var svg = d3.select("svg");
+  var width = +svg.attr("width");
+  var height = +svg.attr("height");
+
+  // color is a function that returns a hexadecimal color when invoked
+  var color = d3.scaleOrdinal(d3.schemeCategory20);
+
+  var simulation = d3.forceSimulation()
+    .force("link", d3.forceLink().id(function (d) { return d.id; }))
+    .force("charge", d3.forceManyBody())
+    .force("center", d3.forceCenter(width / 2, height / 2));
+
+  d3.json("miserables.json", function (error, graph) {
+    if (error) throw error;
+
+    var linkNames = doc.links();
+
+    // iterate through linkNames and make key of x and key of y
+    linkNames.forEach(link => {
+      link.x = 100;
+      link.y = 100;
+    })
+    console.log("wiki", linkNames);
+    console.log("graph", graph.nodes);
+
+    var link = svg.append("g")
+      .attr("class", "links")
+      .selectAll("line")
+      .data(graph.links)
+      .enter().append("line")
+      .attr("stroke-width", function (d) { return Math.sqrt(d.value); });
+
+    var node = svg.append("g")
+      .attr("class", "nodes")
+      .selectAll("g")
+      .data(linkNames)
+      .enter().append("g")
+
+    var circles = node.append("circle")
+      .attr("r", 5)
+      .attr("fill", function (d) { return color(d.group); })
+      .call(d3.drag()
+        .on("start", dragstarted)
+        .on("drag", dragged)
+        .on("end", dragended));
+
+    var lables = node.append("text")
+      .text(function (d) {
+        return d.text;
+      })
+      .attr('x', 6)
+      .attr('y', 3);
+
+    node.append("title")
+      .text(function (d) { return d.text; });
+
+    simulation
+      .nodes(graph.nodes)
+      .on("tick", ticked);
+
+    simulation.force("link")
+      .links(graph.links);
+
+    function ticked() {
+      link
+        .attr("x1", function (d) { return d.source.x; })
+        .attr("y1", function (d) { return d.source.y; })
+        .attr("x2", function (d) { return d.target.x; })
+        .attr("y2", function (d) { return d.target.y; });
+
+      node
+        .attr("transform", function (d) {
+          return "translate(" + d.x + "," + d.y + ")";
+        })
+    }
+  });
+
+  function dragstarted(d) {
+    if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+    d.fx = d.x;
+    d.fy = d.y;
+  }
+
+  function dragged(d) {
+    d.fx = d3.event.x;
+    d.fy = d3.event.y;
+  }
+
+  function dragended(d) {
+    if (!d3.event.active) simulation.alphaTarget(0);
+    d.fx = null;
+    d.fy = null;
+  }
+}
+
 document.querySelector('input').value = "Gold";
 
 let startButton = document.getElementById("begin");
 
 startButton.onclick = start;
+startButton.onclick = startSecond;
 
 document.addEventListener('keydown', e => {
   if (e.code === "Enter") {
@@ -105,4 +209,8 @@ document.addEventListener('keydown', e => {
   }
 });
 
-startButton.click();
+start();
+startSecond();
+
+
+
