5849d809c952e3c70bf71844ca206479b1cccef3
AJ Gosling <ajgosling2@gmail.com>
2020-01-06 19:54:12 -0800

    setting up node iteration

--- app/start_node.js
+++ app/start_node.js
@@ -1,99 +1,10 @@
-// async function startMiserables() {
-//   var svg = d3.select(".legit");
-//   var width = +svg.attr("width");
-//   var height = +svg.attr("height");
-
-//   // color is a function that returns a hexadecimal color when invoked
-//   var color = d3.scaleOrdinal(d3.schemeCategory20);
-
-//   var simulation = d3.forceSimulation()
-//       .force("link", d3.forceLink().id(function (d) { return d.id; }))
-//       .force("charge", d3.forceManyBody())
-//       .force("center", d3.forceCenter(width / 2, height / 2))
-
-//   d3.json("miserables.json", function (error, graph) {
-//       if (error) throw error;
-
-//       var link = svg.append("g")
-//           .attr("class", "links")
-//           .selectAll("line")
-//           .data(graph.links)
-//           .enter().append("line")
-//           .attr("stroke-width", function (d) { return Math.sqrt(d.value); });
-
-//       var node = svg.append("g")
-//           .attr("class", "nodes")
-//           .selectAll("g")
-//           .data(graph.nodes)
-//           .enter().append("g");
-
-//       var circles = node.append("circle")
-//           .attr("r", 5)
-//           .attr("fill", function (d) { return color(d.group); })
-//           .call(d3.drag()
-//               .on("start", dragstarted)
-//               .on("drag", dragged)
-//               .on("end", dragended));
-
-//       var lables = node.append("text")
-//           .text(function (d) {
-//               return d.id;
-//           })
-//           .attr('x', 6)
-//           .attr('y', 3);
-
-//       node.append("title")
-//           .text(function (d) { return d.id; });
-
-//       simulation
-//           .nodes(graph.nodes)
-//           .on("tick", ticked);
-
-//       simulation.force("link")
-//           .links(graph.links);
-
-//       function ticked() {
-//           link
-//               .attr("x1", function (d) { return d.source.x; })
-//               .attr("y1", function (d) { return d.source.y; })
-//               .attr("x2", function (d) { return d.target.x; })
-//               .attr("y2", function (d) { return d.target.y; });
-
-//           node
-//               .attr("transform", function (d) {
-//                   return "translate(" + d.x + "," + d.y + ")";
-//               });
-//       }
-//   });
-
-//   function dragstarted(d) {
-//       if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-//       d.fx = d.x;
-//       d.fy = d.y;
-//   }
-
-//   function dragged(d) {
-//       d.fx = d3.event.x;
-//       d.fy = d3.event.y;
-//   }
-
-//   function dragended(d) {
-//       if (!d3.event.active) simulation.alphaTarget(0);
-//       d.fx = null;
-//       d.fy = null;
-//   }
-
-// }
 
 
-
-
-
-
+const nodes = [];
 const links = [];
 let chain_length = 1;
 
-async function startWiki(nodes = []) {
+async function startWiki(startName) {
 
   let startName = document.querySelector('input').value;
   let doc = await wtf.fetch(startName, "en");
@@ -125,17 +36,42 @@ async function startWiki(nodes = []) {
     group: 1
   };
 
+  console.log(nodes);
 
   if (nodes.length === 0) {
     nodes.push(startPageObject);
   }
 
-
+  // get new set of links
+  // iterate through nodelist and check if link is already present
+  // if it is, make a link for the node that already exists
+  // else, shovel a new node into nodes
 
 
   // // iterate through linkNames and make key of x and key of y
 
   doc.links().forEach((link) => {
+    nodes.forEach((node) => {
+      if (node.id == link.page) {
+        console.log("here");
+      }
+    })
+  })
+
+  doc.links().forEach((link) => {
+    exists = false;
+    nodes.forEach((node) => {
+      if (link.page === node.id) {
+        // node already exists, set up a link between them
+        links.push({
+          source: node.id,
+          group: chain_length
+        })
+        exists = true;
+        break;
+      }
+    });
+  }
     nodes.push({
       "id": link.page,
       "group": chain_length
